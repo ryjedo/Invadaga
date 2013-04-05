0fd952ccb86237bebd25e00b3512375236feed66
findangle <ryjedo@gmail.com>
2013-04-04 21:40:57 -0600

    added esc/pause

--- Assets/Scripts/playerMgr.cs
+++ Assets/Scripts/playerMgr.cs
@@ -10,20 +10,27 @@ public class playerMgr : MonoBehaviour
 	public int score;
 	public int multiShot = 0;
 	public float multiShotFuel = 0;
-	
+	public int escMenuOpen = 0;
 	
 	
 	public void spawnPlayer()
 	{
 		if(lives > 1)
+		{
 			Instantiate(player, new Vector3 (0,0,spawnLocZ), Quaternion.identity);
 			multiShotFuel = 0;
+		}
+		else
+		{
+			escMenuOpen = 1;
+		}
 		//Application.Quit();
 		//spawn gameover ui
 	}
 
 	void Start () 
 	{
+		Time.timeScale = 1;
 		Invoke("spawnPlayer", 0);
 		InvokeRepeating("checkMultiShootFuel",1,1);
 		//Instantiate(player, new Vector3 (0,0,spawnLocZ), Quaternion.identity);
@@ -31,6 +38,23 @@ public class playerMgr : MonoBehaviour
 	}
 	void Update ()
 	{
+
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (escMenuOpen == 0)
+			{
+				escMenuOpen = 1;
+			}
+			else
+			{
+				escMenuOpen = 0;
+				Time.timeScale = 1;
+			}
+		}
+		
+		
+		
+		
 		if(multiShotFuel > 0)
 		{
 			if(multiShot < 1)
@@ -38,6 +62,7 @@ public class playerMgr : MonoBehaviour
 				Invoke("enableMultiShot",0);
 			}
 		}
+
 		if (multiShotFuel < 1)
 		{
 			if(multiShot > 0)
@@ -45,6 +70,7 @@ public class playerMgr : MonoBehaviour
 				Invoke("disableMultiShot",0);
 			}
 		}
+		
 	}
 	
 	//gui stuff
@@ -59,6 +85,28 @@ public class playerMgr : MonoBehaviour
 		//MultiShot
 		GUI.Label(new Rect(50,((Screen.height/2)-40),60,20),"MultiShot =");
 		GUI.Label(new Rect(140,((Screen.height/2)-40),60,20), multiShotFuel.ToString());
+		
+		//esc menu
+		//if(Input.GetKey(KeyCode.Escape))
+		//{
+			
+		//}
+		
+		//dead screen
+		if(escMenuOpen == 1)
+		{
+			Time.timeScale = 0;
+			if(GUI.Button (new Rect((Screen.width/2),(Screen.height/2),60,20),"Restart"))
+			{
+				Application.LoadLevel("character test");
+				lives = 3;
+			}
+
+			if(GUI.Button (new Rect((Screen.width/2),((Screen.height/2)-20),60,20),"Quit"))
+			{
+				Application.Quit();
+			}
+		}
 	}
 		
 	public void enableMultiShot()
