513f6ab68eb43da6e6ad9ad656c36dd2b5470d47
findangle <ryjedo@gmail.com>
2013-04-25 22:40:01 -0600

    Adding WTFPL V2 License

--- Assets/Scripts/sceneManager.cs
+++ Assets/Scripts/sceneManager.cs
@@ -1,3 +1,9 @@
+/*Copyright © 2013 Ryan Dobrynski <ryjedo@gmail.com>
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the LICENSE file for more details.
+*/
+
 using UnityEngine;
 using System.Collections;
 
