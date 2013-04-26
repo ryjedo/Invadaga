76fe7b32ae9f27fb164c55e8828d178f24d3e8f7
findangle <ryjedo@gmail.com>
2013-04-25 22:58:26 -0600

    Code Map

---
+++ Assets/Scripts/CodeMap.mm
@@ -0,0 +1,65 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Scripts" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1366951455942"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2"/>
+<node TEXT="sceneManager" POSITION="right" ID="ID_871519388" CREATED="1366951472432" MODIFIED="1366951819796">
+<edge COLOR="#0000ff"/>
+<node TEXT="playerPowerupFuelDrain" ID="ID_980509761" CREATED="1366951557632" MODIFIED="1366951734552">
+<node TEXT="drains shield, multishot and weaponheat" ID="ID_763507516" CREATED="1366951830709" MODIFIED="1366951843770"/>
+</node>
+<node TEXT="gui" ID="ID_310125316" CREATED="1366951754763" MODIFIED="1366951757287"/>
+<node TEXT="player vars" ID="ID_1995523074" CREATED="1366951767572" MODIFIED="1366951773424">
+<node TEXT="score" ID="ID_510197952" CREATED="1366951778259" MODIFIED="1366951780215"/>
+<node TEXT="lives" ID="ID_1813878913" CREATED="1366951785610" MODIFIED="1366951793551"/>
+</node>
+</node>
+</node>
+</map>
